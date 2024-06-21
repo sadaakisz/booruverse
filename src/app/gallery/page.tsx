c495e9bf62b8a417d1cbc627555bc88715f95e39
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-20 22:41:48 -0500

    Built foundation for the gallery component.
    Implemented a masonry gallery with a mapped list to show images from URL stored in a list.
    TODO: Convert this into a component that can receive a URL list.

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+
+export default function Page() {
+
+    const imagesURLList = [
+        "https://testbooru-cdn.donmai.us/original/ac/5e/ac5ee35f708f2d78b752263cab8f8ed3.jpg",
+        "https://testbooru-cdn.donmai.us/sample/ab/7a/sample-ab7a8707cbe7b6af6a67c4007b6f16a5.jpg",
+        "https://testbooru-cdn.donmai.us/sample/d4/86/sample-d486dd141db10be5f0770bb0e4f23950.jpg",
+        "https://testbooru-cdn.donmai.us/sample/f9/39/sample-f9392964ff34cc0115497746b221202f.jpg",
+        "https://testbooru-cdn.donmai.us/sample/4d/af/sample-4daf54039c040b02b76f1f2937605208.jpg",
+        "https://testbooru-cdn.donmai.us/sample/70/aa/sample-70aa1aa95900b053c6edefff5da3ba7d.jpg",
+        "https://testbooru-cdn.donmai.us/original/47/cd/47cdf772be0433ece14c970517a277cf.jpg",
+        "https://testbooru-cdn.donmai.us/sample/46/97/sample-469799de68d48c94cc9d61d35f6d14df.jpg",
+        "https://testbooru-cdn.donmai.us/sample/76/41/sample-7641d7b58f0d34b7962f053e845da28a.jpg",
+        "https://testbooru-cdn.donmai.us/sample/5d/ee/sample-5dee9cb1e8697d54f026d6442009a4f1.jpg",
+        "https://testbooru-cdn.donmai.us/sample/6b/3b/sample-6b3b1a79aa05eea62d5b4c44a9d94273.jpg",
+        "https://testbooru-cdn.donmai.us/sample/52/6c/sample-526c1cb458669e9696f2e2ce4857c948.jpg",
+        "https://testbooru-cdn.donmai.us/sample/bc/d4/sample-bcd4253f4ee4bdc464825f2d5e7c3778.jpg",
+        "https://testbooru-cdn.donmai.us/sample/71/4b/sample-714bad19b70d93266e6f49f3cf56ca31.jpg",
+        "https://testbooru-cdn.donmai.us/sample/3b/2e/sample-3b2eb9c7f4e06f6de67a863b7adf05ff.jpg",
+        "https://testbooru-cdn.donmai.us/sample/df/5e/sample-df5eae4a9f41eaa1dbd0ee95635bc7ab.jpg"
+    ]
+
+    const picsumURLList = [
+        "https://picsum.photos/200/300",
+        "https://picsum.photos/400/300",
+        "https://picsum.photos/200/600",
+        "https://picsum.photos/300/700",
+        "https://picsum.photos/250/450",
+        "https://picsum.photos/300/500",
+        "https://picsum.photos/400/900",
+        "https://picsum.photos/500/900",
+        "https://picsum.photos/600/300",
+        "https://picsum.photos/700/200",
+        "https://picsum.photos/200/400",
+        "https://picsum.photos/700/300",
+        "https://picsum.photos/500/300",
+        "https://picsum.photos/900/900",
+        "https://picsum.photos/500/900",
+        "https://picsum.photos/750/300"
+    ]
+
+    // masonry grid: https://tailwindflex.com/@simon-scheffer/masonry-grid
+    return (
+        <div /* className="p-5 sm:p-8" */>
+            <div className="columns-1 gap-3 sm:columns-2 md:columns-3 lg:columns-4 [&>Image:not(:first-child)]:mt-8">
+                {/* width auto fill: https://stackoverflow.com/a/76008677 */}
+                {/* map loop urls: https://stackoverflow.com/a/71834386*/}
+                { picsumURLList.map((imgURL, i) => (
+                    <div key={i}>
+                        <Image 
+                            className="mb-3 rounded-xl"
+                            src={imgURL} 
+                            alt="" 
+                            width={0} height={0} sizes="100vw" 
+                            style={{ width: '100%', height: 'auto' }}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
