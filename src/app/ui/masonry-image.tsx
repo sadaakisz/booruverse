9e5efcaf308f0e3709b1816f901b2cfc07add99f
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-22 15:50:53 -0500

    Comments and adjustments to Masonry components.

--- src/app/ui/masonry-image.tsx
+++ src/app/ui/masonry-image.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-// width auto fill: https://stackoverflow.com/a/76008677
+// KNOWLEDGE: width auto fill: https://stackoverflow.com/a/76008677
 export default function MasonryImage({ imageURL }: { imageURL: string }) {
     return (
         <Image
-            className="mb-3 rounded-xl"
+            className="rounded-xl"
             src={imageURL}
             alt=""
             width={0} height={0} sizes="100vw"
--- src/app/ui/masonry-video.tsx
+++ src/app/ui/masonry-video.tsx
@@ -1,8 +1,8 @@
-// video component: https://imagekit.io/blog/nextjs-video-player/
-// research a way to autoplay mute and unmute on click (like TikTok and Instagram)
+// KNOWLEDGE: video component: https://imagekit.io/blog/nextjs-video-player/
+// KNOWLEDGE: research a way to autoplay mute and unmute on click (like TikTok and Instagram)
 export default function MasonryVideo({ videoURL }: { videoURL: string }) {
     return (
-        <video className="mb-3 rounded-xl" controls playsInline>
+        <video className="rounded-xl" controls playsInline>
             <source src={videoURL} />
             Your browser does not support the video tag...
         </video>
