68dd730e01d4bfd5b9a9d437d0752cec47b90a55
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-22 15:52:57 -0500

    Implemented MasonryGalleries with dynamic column number.
    colsNum can be changed to adapter a different column number Masonry. Also implemented so more info is gathered from the JSON and passed to the components.

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,38 +1,35 @@
 import MasonryGallery from "../ui/masonry-gallery";
+import MasonryGallery2c from "../ui/masonry-gallery-2c";
+import MasonryGallery3c from "../ui/masonry-gallery-3c";
+import MasonryGallery4c from '../ui/masonry-gallery-4c';
+import { BooruMedia, BooruMediaSource } from '../lib/definitions';
 
-const url = "https://testbooru.donmai.us/posts.json?page=1&limit=40";
+const url = "https://testbooru.donmai.us/posts.json?page=10&limit=40";
 
-async function getImageURLList() {
+// KNOWLEDGE: how to reduce specific object parameters: https://stackoverflow.com/a/60408924
+async function getBooruMediaArray() {
     const response = await fetch(url);
     const data = await response.json();
-    const urlArr = data.map((imageObj: any) => imageObj.file_url);
-    const cleanUrlArr = urlArr.filter((url: string) => typeof url !== "undefined");
-    //console.log(cleanUrlArr);
-    return cleanUrlArr;
+    const booruMediaArr: Array<BooruMedia> = data.map((media: any) => (
+        {
+            id: media.id,
+            score: media.score,
+            rating: media.rating,
+            file_ext: media.file_ext,
+            tag_string_general: media.tag_string_general,
+            tag_string_character: media.tag_string_character,
+            tag_string_artist: media.tag_string_artist,
+            tag_string_meta: media.tag_string_meta,
+            file_url: media.file_url,
+        }
+    ));
+    const cleanBooruMediaArr = booruMediaArr.filter((media: BooruMedia) => typeof media.file_url !== "undefined");
+    return cleanBooruMediaArr;
 }
-
+// TODO: Search how to implement skeletons and shimer components using the dimensions when loading.
 export default async function Page() {
-
-    const testbooruURLList = [
-        "https://testbooru-cdn.donmai.us/original/ac/5e/ac5ee35f708f2d78b752263cab8f8ed3.jpg",
-        "https://testbooru-cdn.donmai.us/sample/ab/7a/sample-ab7a8707cbe7b6af6a67c4007b6f16a5.jpg",
-        "https://testbooru-cdn.donmai.us/sample/d4/86/sample-d486dd141db10be5f0770bb0e4f23950.jpg",
-        "https://testbooru-cdn.donmai.us/sample/f9/39/sample-f9392964ff34cc0115497746b221202f.jpg",
-        "https://testbooru-cdn.donmai.us/sample/4d/af/sample-4daf54039c040b02b76f1f2937605208.jpg",
-        "https://testbooru-cdn.donmai.us/sample/70/aa/sample-70aa1aa95900b053c6edefff5da3ba7d.jpg",
-        "https://testbooru-cdn.donmai.us/original/47/cd/47cdf772be0433ece14c970517a277cf.jpg",
-        "https://testbooru-cdn.donmai.us/sample/46/97/sample-469799de68d48c94cc9d61d35f6d14df.jpg",
-        "https://testbooru-cdn.donmai.us/sample/76/41/sample-7641d7b58f0d34b7962f053e845da28a.jpg",
-        "https://testbooru-cdn.donmai.us/sample/5d/ee/sample-5dee9cb1e8697d54f026d6442009a4f1.jpg",
-        "https://testbooru-cdn.donmai.us/sample/6b/3b/sample-6b3b1a79aa05eea62d5b4c44a9d94273.jpg",
-        "https://testbooru-cdn.donmai.us/sample/52/6c/sample-526c1cb458669e9696f2e2ce4857c948.jpg",
-        "https://testbooru-cdn.donmai.us/sample/bc/d4/sample-bcd4253f4ee4bdc464825f2d5e7c3778.jpg",
-        "https://testbooru-cdn.donmai.us/sample/71/4b/sample-714bad19b70d93266e6f49f3cf56ca31.jpg",
-        "https://testbooru-cdn.donmai.us/sample/3b/2e/sample-3b2eb9c7f4e06f6de67a863b7adf05ff.jpg",
-        "https://testbooru-cdn.donmai.us/sample/df/5e/sample-df5eae4a9f41eaa1dbd0ee95635bc7ab.jpg"
-    ]
-
-    const picsumURLList = [
+    const colsNum: Number = 4;
+    const picsumURLArray = [
         "https://picsum.photos/200/300",
         "https://picsum.photos/400/300",
         "https://picsum.photos/200/600",
@@ -51,11 +48,21 @@ export default async function Page() {
         "https://picsum.photos/750/300"
     ]
 
-    const imagesURLList = await getImageURLList();
+    const booruMediaArray = await getBooruMediaArray();
+    const booruMediaSourceArray: Array<BooruMediaSource> = booruMediaArray.map((media: BooruMedia) => ({
+        file_url: media.file_url,
+        file_ext: media.file_ext
+    }));
     return (
         <div>
-            {/* <button onClick={e => callAPIWithURL()}>Make API Call</button> */}
-            <MasonryGallery imageURLList={ imagesURLList }/>
+            { colsNum == 2
+                ? <MasonryGallery2c booruMediaSourceArray={ booruMediaSourceArray }/>
+                : colsNum == 3
+                    ? <MasonryGallery3c booruMediaSourceArray={ booruMediaSourceArray }/>
+                    : colsNum == 4
+                        ? <MasonryGallery4c booruMediaSourceArray={ booruMediaSourceArray }/>
+                        : <h1>Invalid colsNum value.</h1>
+            }
         </div>
     );
 }
