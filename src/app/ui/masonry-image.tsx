3a638f3ee612c92eb29bd147974ec648cde00679
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-22 10:31:52 -0500

    Space lint adjustment.

--- src/app/ui/masonry-image.tsx
+++ src/app/ui/masonry-image.tsx
@@ -5,9 +5,9 @@ export default function MasonryImage({ imageURL }: { imageURL: string }) {
     return (
         <Image
             className="mb-3 rounded-xl"
-            src={imageURL} 
-            alt="" 
-            width={0} height={0} sizes="100vw" 
+            src={imageURL}
+            alt=""
+            width={0} height={0} sizes="100vw"
             style={{ width: '100%', height: 'auto' }}
         />
     );
