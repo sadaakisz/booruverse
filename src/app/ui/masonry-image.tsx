980a502af02f0fcf2525305d03cd63a258857b71
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-22 17:59:26 -0500

    Implemented fade-in animation.

--- src/app/ui/masonry-image.tsx
+++ src/app/ui/masonry-image.tsx
@@ -1,14 +1,21 @@
+'use client';
+
 import Image from "next/image";
+import { useState } from "react";
 
 // KNOWLEDGE: width auto fill: https://stackoverflow.com/a/76008677
+// KNOWLEDGE: skeletons: https://stackoverflow.com/a/77031188
+// decided not to go the skeleton route with onLoad, because it's firing early on some images.
 export default function MasonryImage({ imageURL }: { imageURL: string }) {
+    const [loaded, setLoaded] = useState(false)
     return (
         <Image
-            className="rounded-xl"
+            className={`${(!loaded) ? 'opacity-0' : 'opacity-100'} rounded-xl`}
+            onLoad={() => setLoaded(true)}
             src={imageURL}
             alt=""
             width={0} height={0} sizes="100vw"
-            style={{ width: '100%', height: 'auto' }}
+            style={{ width: '100%', height: 'auto', transition: 'opacity 0.7s ease-out' }}
         />
     );
 }
--- src/app/ui/masonry-video.tsx
+++ src/app/ui/masonry-video.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 // KNOWLEDGE: video component: https://imagekit.io/blog/nextjs-video-player/
 // KNOWLEDGE: research a way to autoplay mute and unmute on click (like TikTok and Instagram)
 export default function MasonryVideo({ videoURL }: { videoURL: string }) {
     return (
-        <video className="rounded-xl" controls playsInline>
+        <video 
+            className="rounded-xl animate-fade-in"
+            controls
+            playsInline
+            >
             <source src={videoURL} />
             Your browser does not support the video tag...
         </video>
