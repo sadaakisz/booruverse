80f3ca66ef92b1250e5888dd8091a430681e1c7f
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-25 20:51:08 -0500

    Implemented column number switching button.

--- src/app/igallery/index-gallery.tsx
+++ src/app/igallery/index-gallery.tsx
@@ -5,9 +5,10 @@ import { BooruMedia } from '../lib/definitions';
 import MasonryGallery2c from "../ui/masonry-gallery-2c";
 import MasonryGallery3c from "../ui/masonry-gallery-3c";
 import MasonryGallery4c from "../ui/masonry-gallery-4c";
+import { Squares2X2Icon } from '@heroicons/react/24/outline';
 
 // BUG: It crashes when hot reloading, don't know if it's affected by another scenario. Solves itself reloading the page.
-// SOLUTION: https://stackoverflow.com/a/75339011
+// SOLUTION?: https://stackoverflow.com/a/75339011
 // Added suppressHydrationWarning={true} in Root Layout. 
 // Sometimes it still happens when changing significant code? Only with extensions on though.
 // It appears that using setRequestedPages instead of pushing directly solves it.
@@ -17,7 +18,7 @@ import MasonryGallery4c from "../ui/masonry-gallery-4c";
 // TODO: Figure out a way to balance columns (math) or begin loading when that smallest column ends (this is kinda a workaround).
 const IndexGallery = ({ initialData }: { initialData: any}) => {
     // TODO: Change this value to be in Page component?
-    const colsNum: Number = 3;
+    const [colsNum, setColsNum] = useState(2);
 
     const [data, setData] = useState(initialData.props.initialData);
     const [booruMediaArray, setBooruMediaArray] = useState(
@@ -78,11 +79,8 @@ const IndexGallery = ({ initialData }: { initialData: any}) => {
             }
             setIsLoading(false);
         };
-        /* console.log('window.innerHeight:', window.innerHeight);
-        console.log('window.scrollY:', window.scrollY);
-        console.log('document.body.offsetHeight:', document.body.offsetHeight); */
-        // TODO: Adjust this condition below to prefetch more images v
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - window.innerHeight && !isLoading) {
+        // KNOWLEDGE: it fetches more pages when 0.75x of the gallery height
+        if (window.scrollY >= document.body.offsetHeight * (3/4) && !isLoading) {
             await loadMoreData();
         }
     }, [data, isLoading, page, requestedPages]); // Dependencies
@@ -91,6 +89,14 @@ const IndexGallery = ({ initialData }: { initialData: any}) => {
     window.addEventListener('scroll', onScroll);
     return () => window.removeEventListener('scroll', onScroll);
     }, [onScroll]);
+
+    function changeColsNum() {
+        console.log('colsNum:', colsNum)
+        if (colsNum == 2) setColsNum(3);
+        else if (colsNum == 3) setColsNum(4);
+        else setColsNum(2);
+    }
+
     return (
         <div>
             { colsNum == 2
@@ -101,7 +107,27 @@ const IndexGallery = ({ initialData }: { initialData: any}) => {
                         ? <MasonryGallery4c booruMediaArray={ booruMediaArray }/>
                         : <h1>Invalid colsNum value.</h1>
             }
-            {isLoading && <p>Loading more images...</p>} {/* Loading indicator */}
+            <div
+            className="flex items-center justify-center fixed bottom-4 right-16 mr-2 h-[48px] w-[48px] bg-gray-900 rounded-xl"
+            >
+                {
+                    !isLoading &&
+                    <h1>{page}</h1>
+                }
+                {
+                    isLoading &&
+                    <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                }
+            </div>
+            <div 
+                className="flex items-center justify-center fixed bottom-4 right-4 h-[48px] w-[48px] bg-gray-900 rounded-xl"
+                onClick={() => changeColsNum()}
+            >
+                <Squares2X2Icon className="h-[24px] w-[24px] text-gray-300 peer-focus:text-gray-900"/>
+            </div>
         </div>
     );
 };
