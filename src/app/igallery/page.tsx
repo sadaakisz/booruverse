56a8b02990165e602a5792e71e5fe5997b745a06
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-25 23:33:22 -0500

    Added TODOs.

--- src/app/igallery/page.tsx
+++ src/app/igallery/page.tsx
@@ -1,5 +1,13 @@
 import IndexGallery from "./index-gallery";
 
+// TODO: Figure out a way to add image to gallery if loaded to prevent those gaps
+// SOLUTION: Made the height of the image 0 if not loaded. It shows unloaded images tho.
+// POSSIBLE WORKAROUND: Pass blurURL as placeholder or pass a lower resolution url (media_asset - variants - [2?] or type "720x720" - url)
+// https://nextjs.org/docs/pages/api-reference/components/image#placeholder
+
+// TODO: Implement safe mode toggle
+// TODO: Implement test and real danbooru calls switch
+
 async function getStaticProps() {
     const res = await fetch('https://testbooru.donmai.us/posts.json?page=1&limit=40&tags=rating:g');
     const initialData = await res.json();
