0dadb46bda2253c46fb5666de0cec85e184fb046
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-27 21:46:13 -0500

    Added TODO.
    // TODO: Consider using large_file_url instead of variants[2]

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,6 +4,7 @@ import IndexGallery from "./index-gallery";
 
 // TODO: Implement safe mode toggle
 // TODO: Implement test and real danbooru calls switch
+// TODO: Consider using large_file_url instead of variants[2]
 
 async function getStaticProps() {
     const res = await fetch('https://testbooru.donmai.us/posts.json?page=1&limit=40&tags=rating:g');
