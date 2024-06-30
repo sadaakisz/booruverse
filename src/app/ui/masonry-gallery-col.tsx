948233bbd40f1c5ef1d3a13305c9a73fc1122efa
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-30 18:58:40 -0500

    Improved image loading performance.
    Images now show the preview image first and then the full res on top when loaded.

--- src/app/ui/masonry-gallery-col.tsx
+++ src/app/ui/masonry-gallery-col.tsx
@@ -15,22 +15,21 @@ export default function MasonryGalleryCol({ booruMediaArray, hq }: { booruMediaA
     return (
         <div className="grid-element space-y-3">
             { booruMediaArray.map((booruMedia: BooruMedia) => (
-                <>
+                <div key={booruMedia.id}>
                     <Link
-                        key={booruMedia.id}
                         href={`/?domain=${booruMedia.domain}&imageId=${booruMedia.id}`}
                         as={`/${booruMedia.domain}/${booruMedia.id}`}
                         shallow
                         className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
                     >
                         { booruMedia.file_ext === "mp4" || booruMedia.file_ext === "webm"
-                            ? <MasonryVideo key={booruMedia.id} videoURL={booruMedia.file_url}/>
+                            ? <MasonryVideo videoURL={booruMedia.file_url}/>
                             : hq
-                                ? <MasonryImageHQ key={booruMedia.id} imageURL={booruMedia.file_url} previewURL={booruMedia.large_file_url}/>
-                                : <MasonryImageLQ key={booruMedia.id} imageURL={booruMedia.file_url} previewURL={booruMedia.large_file_url}/>
+                                ? <MasonryImageHQ imageURL={booruMedia.file_url} previewURL={booruMedia.large_file_url}/>
+                                : <MasonryImageLQ imageURL={booruMedia.file_url} previewURL={booruMedia.large_file_url}/>
                         }
                     </Link>
-                </>
+                </div>
             ))}
         </div>
     );
