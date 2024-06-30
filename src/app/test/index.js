2239f902a08c3ae95bed0fd28b66e45b4ff08939
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-06-29 22:32:47 -0500

    Removed unused experimental code.

--- src/app/test/index.js
+++
@@ -1,6 +0,0 @@
-// pages/index.js
-export async function getStaticProps() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/photos?_limit=10');
-    const initialData = await res.json();
-    return { props: { initialData } };
-  }
